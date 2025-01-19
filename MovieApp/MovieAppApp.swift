f7f73f7bcf47dfe43050c1cbacaef7096cc30b11
layanasw <lyan.wasaidi2@gmail.com>
2025-01-19 15:10:06 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by layan alwasaidi on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
